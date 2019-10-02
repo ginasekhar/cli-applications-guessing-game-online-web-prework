749102379e0cca0e615aaab7a6bcb0c53a4f9692
Gina Sekhar <ginasekhar@yahoo.com>
2019-10-02 18:58:28 +0000

    Automatically backed up by Learn

--- guessing_game_cli.rb
+++ guessing_game_cli.rb
@@ -9,7 +9,7 @@ def run_guessing_game
   end
   user_guess_num = user_guess.to_i
   if user_guess_num == num
-    puts "You guessed the correct number"
+    puts "You guessed the correct number!"
   else
     puts "Sorry! The computer guessed #{num}"
   end
